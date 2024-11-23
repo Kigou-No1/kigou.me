143096d3082b66d907259187fd3ffa40a286ccdc
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-11-23 16:20:29 +0900

    titleの上限上げた

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const blogCollection = defineCollection({
     type: "content",
     schema: z.object({
         emoji: z.string().emoji(),
-        title: z.string().max(20).min(1),
+        title: z.string().max(100).min(1),
         draft: z.boolean(),
         uploadAt: z.date(),
         tags: z.array(z.string().max(20).min(1)).max(5).min(0).optional(),
