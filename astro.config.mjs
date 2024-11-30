12148af87a2e6c1c8ec9e1cbd910a0116ea86f9f
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-12-01 01:15:56 +0900

    アクセシビリティを向上

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,16 @@ import tailwind from '@astrojs/tailwind';
 import { fileURLToPath } from "url";
 import path, { dirname } from 'path';
 
+import sitemap from "@astrojs/sitemap";
+
+import robots from "astro-robots";
+
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = dirname(__fileName)
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap(), robots()],
   site: "https://kigou.me",
   trailingSlash: "never",
   build: {
